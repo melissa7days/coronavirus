075488a2277eec86d95bbd4f59cd353209642be8
melissagovender9701 <melissa.govender9701@gmail.com>
2020-04-05 19:04:14 +0200

    videos and contact

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule,NO_ERRORS_SCHEMA } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopNavComponent } from './components/top-nav/top-nav.component';
 import { StatsComponent } from './components/stats/stats.component';
 import { SafetyComponent } from './components/safety/safety.component';
-
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { VideoComponent } from './components/video/video.component';
+import { ContactComponent } from './components/contact/contact.component';
 @NgModule({
   declarations: [
     AppComponent,
     TopNavComponent,
     StatsComponent,
     SafetyComponent,
+    VideoComponent,
+    ContactComponent,
   ],
   imports: [
     HttpClientModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MDBBootstrapModule.forRoot()
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [ NO_ERRORS_SCHEMA ]
 })
 export class AppModule { }
